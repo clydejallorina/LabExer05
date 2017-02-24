7bd66fabd6b6704a013333c5cb83f821aa68fe49
clydejallorina <clydejallorina@gmail.com>
2017-02-24 20:50:15 +0800

    Update FXMLDocumentController.java

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -36,13 +36,18 @@ public class FXMLDocumentController implements Initializable {
                 throw new InvalidInputException(temp);
             }
             else {
-                outputLabel.setText(temp + " deg C = " + new DecimalFormat("#0.00").format((temp * 1.8) + 32) + " deg F");
+                outputLabel.setText(temp + " deg C = " + new DecimalFormat("#0.00").format((temp * 1.8) + 32) + " deg F"); 
+                //compressed everything to one line so let me explain...
+                /* All I did here was do the conversion inside
+                 * the format function of the DecimalFormat object.
+                 * Everything else is just formatting.
+                 */
             }
         }
-        catch (InvalidInputException e) {
+        catch (InvalidInputException e) { //if the code above throws IIE, do this
             outputLabel.setText(e.getMessage());
         }
-        catch (NumberFormatException e) {
+        catch (NumberFormatException e) { //if the code above throws NFE, do this
             outputLabel.setText("'" + input.getText() + "' is not a valid format!");
             System.out.println(e);
         }
